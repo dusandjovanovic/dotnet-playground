fa9f9cbe351b586dfd99c44aee714707e1ce9b33
Dusan Jovanovic <dusandjovanovic@outlook.com>
2020-03-30 14:22:27 +0200

    .NET Console playground

---
+++ dotnet-shell/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dotnet_shell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = FeetToInches(10);
+            System.Console.WriteLine(x);
+
+            string message = "Hello world!";
+            string upperMessage = x.ToString() + " " + message.ToUpper();
+            Console.WriteLine(upperMessage);
+
+            bool lessThan120 = x < 120;
+            Console.WriteLine(lessThan120);
+        }
+
+        static int FeetToInches(int feet)
+        {
+            int @inches = feet * 12;
+            return inches;
+        }
+    }
+}
