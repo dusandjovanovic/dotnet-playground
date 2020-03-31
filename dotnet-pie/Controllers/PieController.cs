2da5a44cbca2fb959f5ff5543edee72b5ccd578e
Dusan Jovanovic <dusandjovanovic@outlook.com>
2020-03-31 05:34:41 +0200

    Model and ModelView

---
+++ dotnet-pie/Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_pie.Models;
+using dotnet_pie.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_pie.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this._pieRepository = pieRepository;
+            this._categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel();
+            pieListViewModel.Pies = _pieRepository.AllPies;
+            pieListViewModel.CurrentCategory = "Cheese cake";
+
+            return View(pieListViewModel);
+        }
+    }
+}
---
+++ dotnet-pie/ViewModels/PieListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using dotnet_pie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_pie.ViewModels
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+
+        public string CurrentCategory { get; set; }
+    }
+}
