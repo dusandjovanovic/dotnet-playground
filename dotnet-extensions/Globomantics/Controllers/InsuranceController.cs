792c65dd603c0f8decc852a7a91f209b42c111e6
Dusan Jovanovic <dusandjovanovic@outlook.com>
2020-04-30 16:58:02 +0200

    Extension points with Filters (Action, Exception and Auth)

---
+++ dotnet-extensions/Globomantics/Controllers/InsuranceController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Globomantics.Models;
+using Globomantics.Core.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Globomantics.Controllers
+{
+    public class InsuranceController : Controller
+    {
+        private ILogger<InsuranceController> logger;
+
+        public InsuranceController(ILogger<InsuranceController> logger)
+        {
+            this.logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            logger.LogInformation("Bad model", contact);
+            return View(contact);
+        }
+
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
---
+++ dotnet-extensions/Globomantics/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Globomantics.Filters;
+using Globomantics.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Globomantics
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(typeof(ModelValidationFilter));
+            });
+            services.AddTransient<IDocumentService, DocumentService>();
+            services.AddSingleton<ILoanService, LoanService>();
+            services.AddTransient<IQuoteService, QuoteService>();
+            services.AddTransient<IFeatureService, FeatureService>();
+            services.AddTransient<IRateService, RateService>();
+
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.Cookie.HttpOnly = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
